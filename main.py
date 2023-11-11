910bbefd120ffcdb0f206b62a9018e88194b503d
jqjqyangjq <njuyangjq@gmail.com>
2023-11-11 03:15:33 +0100

    updates_params

--- main.py
+++ main.py
@@ -33,7 +33,9 @@ def main():
     train_subparser.add_argument("-maximum_dep", metavar='', help = "max depth for per position", type = int, default = None)
     train_subparser.add_argument("-minimum_dep", metavar='', help = "min depth for per position", type = int, default = None)
     train_subparser.add_argument("-not_est_transition",  action='store_true', help = "estimate transition parameter or not", default= False)
-    
+    train_subparser.add_argument("-transition_1", metavar='',  help = "transition_param_1", default= None, type=float)
+    train_subparser.add_argument("-transition_2", metavar='',  help = "transition_param_2", default= None, type=float)
+
     decode_subparser = subparser.add_parser('decode', help='Decode posterior from params trained')
     decode_subparser.add_argument("-filter_depth", action='store_true', help = "whetther set a uniform filter on coverage", default = False)
     decode_subparser.add_argument("-gll_file", metavar='', 
@@ -62,13 +64,17 @@ def main():
             parser.print_help()
             return
         hmm_parameters = read_HMM_parameters_from_file(args.param)
+        if args.transition_1 is not None:
+            hmm_parameters[0] = [1- args.transition_1, args.transition_1]
+        if args.transition_2 is not None:
+            hmm_parameters[1] = [args.transition_2, 1 - args.transition_2]
+        print(hmm_parameters)
         print(f"filter depth: {args.filter_depth}")
         print(f"fixed transition parameters: {args.not_est_transition}")
         print(args.param)
         #obs, _, _, _, mutrates, weights = Load_observations_weights_mutrates(args.obs, args.weights, args.mutrates, args.window_size, args.haploid)
         observation, chrs, windows, obs_count = load_observations(args.gll_file, args.window_size, args.filter_depth, args.maximum_dep, args.minimum_dep)
         print('-' * 40)
-        print(hmm_parameters)
         print('> Output is',args.out) 
         print('> Window size is',args.window_size, 'bp') 
         print('-' * 40)
